525be9dea1ef5b1018fbcd959658957ea51b90c5
Nowroz Shahriar <nowroz.shahriar@gmail.com>
2021-11-19 12:08:19 +0600

    1.4.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { MongoClient, Collection } = require('mongodb');
-// const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const cors = require('cors');
 
@@ -12,12 +12,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.skciu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// client.connect(err => {
-//     const collection = client.db("test").collection("devices");
-//     // perform actions on the collection object
-//     client.close();
-//   });
 
 async function run() {
     try {
@@ -32,6 +26,21 @@ async function run() {
             const cursor = productsCollection.find({});
             const products = await cursor.toArray();
             res.send(products);
+        })
+
+        // POST Products API
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.json(result);
+        })
+
+        // DELETE Products API
+        app.delete('/products', async (req, res) => {
+            const id = req.body.id;
+            const query = {_id:ObjectId(id)};
+            const result = await productsCollection.deleteOne(query);
+            res.json(result);
         })
 
         // POST Orders API
@@ -50,14 +59,41 @@ async function run() {
             res.json(orders);
         })
 
+        // DELETE Orders API
+        app.delete('/orders', async (req, res) => {
+            const id = req.body.id;
+            const query = {_id:ObjectId(id)};
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
+
         // POST Users API
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
-            console.log(result);
             res.json(result);
         })
 
+        // PUT Admin API
+        app.put('/users', async (req, res) => {
+            const email = req.body.email;
+            const filter = {email};
+            const updateDoc = {$set: {role: 'admin'}};
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+
+        // GET Admin API
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {email};
+            let isAdmin = false;
+            const user = await usersCollection.findOne(query);
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({admin: isAdmin})
+        })
 
 
 
